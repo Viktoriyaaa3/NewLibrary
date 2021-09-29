bc1bae7aa153c1d5931af55c3ea57df2145cd06e
Viktoriyaaa3 <tulupova5@gmail.com>
2021-09-29 11:32:18 +0200

    Cerco di risolvere problemi vista/controller per il Login

--- src/main/java/teoresiGroup/web/controller/LoginController.java
+++ src/main/java/teoresiGroup/web/controller/LoginController.java
@@ -1,13 +1,21 @@
 package teoresiGroup.web.controller;
 
+import org.springframework.ui.Model;
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import teoresiGroup.web.model.UtentiModel;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	private final static Logger log= Logger.getLogger(LoginController.class.getName());
 	/*pag di registrazione è get, il submit è il post*/
 	/*GETmAPPING= definisce metodo accessibile solo via get
 	 * PostMapping:
@@ -41,19 +49,31 @@ public class LoginController {
 
 /*usare redirect quando voglio inviare la richiesta a un'altra pagina o uun altro controller. e forward per indirizzare più richieste*/
 
-	@GetMapping
-	public String logIn() {
+	@GetMapping("/log")
+	public ModelAndView logIn(Model model) {
+		UtentiModel utenti= new UtentiModel();
+		model.addAttribute("utenteForm", utenti);
+		
 		/*
 		 * 
 		 * if(password e username esiste)
 		 * return :tiSeiLoggato
 		 * 
 		 * else return pagina di registrazione*/
-		return "tiSeiLoggato";
+		return new ModelAndView("login", "utenteForm", new UtentiModel());
+		
 	}
 	
-	@PostMapping
-	public String logOk() {
+	@PostMapping("/controllo")
+	public String logOk(@RequestParam("utentiForm") String password, @RequestParam("utentiForm.username") String username, Model model) {
+		log.info("Controllo i dati che mi stanno arrivando dall'utente: ");
+		model.addAttribute("utentiForm.password", password);
+		model.addAttribute("utentiForm.username", username);
+		
+		if(password!=null && username!=null)
+		{
+			log.info("I dati che sono arrivati: " + password + " " + username );
+		}
 		
 		return "redirect/welcomePage";
 	}
