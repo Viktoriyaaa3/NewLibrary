0696f7be5d9c3f1e04d819ca3c330c9a6b67eba5
Viktoriyaaa3 <tulupova5@gmail.com>
2021-10-21 18:43:20 +0200

    prova

--- src/main/java/teoresiGroup/web/security/SecurityConfig.java
+++ src/main/java/teoresiGroup/web/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package teoresiGroup.web.security;
 
 import javax.sql.DataSource;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,19 +14,24 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	private static final Logger log= Logger.getLogger(SecurityConfig.class.getName());
+	
 	@Autowired 
 	private DataSource dataSource;
 	
 
-
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		log.info(auth);
+		UserBuilder users=User.withDefaultPasswordEncoder();
 		
-		auth.jdbcAuthentication().dataSource(dataSource);
+		auth.inMemoryAuthentication().withUser(users.username("vik")
+				.password("vik").roles("EMPLOYEE"))
+		.withUser(users.username("john")
+				.password("test123").roles("EMPLOYEE"))
+		.withUser(users.username("admin")
+				.password("admin").roles("ADMIN"));
 		
-		log.info(dataSource + "getAuth " + auth);
+		
 		
 	}
 
